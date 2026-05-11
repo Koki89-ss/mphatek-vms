83d4ce593da7fe1d8c723d93a23942e516b35e3a
Koketso <boipelokoki77@gmail.com>
2026-05-11 13:21:41 +0200

    Updated the resetreception.js to use pool

--- server/resetreception.js
+++ server/resetreception.js
@@ -1,15 +1,14 @@
 // save as resetReception.js in your server folder
 const bcrypt = require("bcrypt");
-const { getPool, sql } = require("./db");
+const pool = require("./db");
 
 async function reset() {
   const hash = await bcrypt.hash("Reception@2026!", 10);
-  const pool = await getPool();
 
-  await pool.request()
-    .input("hash", sql.NVarChar, hash)
-    .input("email", sql.NVarChar, "busisiwe.zikhalala@mphatek.com")
-    .query("UPDATE Users SET PasswordHash = @hash WHERE Email = @email");
+  await pool.query(
+    "UPDATE Users SET PasswordHash = $1 WHERE Email = $2",
+    [hash, "busisiwe.zikhalala@mphatek.com"]
+  );
 
   console.log("✅ Reception password updated.");
   process.exit(0);
