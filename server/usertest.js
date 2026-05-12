c99deb47c1a780e2abca6f5f12903ba0177ac73e
Koketso <boipelokoki77@gmail.com>
2026-05-12 12:50:24 +0200

    Update usertest.js withtwo new employee

--- server/usertest.js
+++ server/usertest.js
@@ -8,20 +8,23 @@ async function seed() {
   const users = [
     { fullName: "Sikhozana Fanelesibonge", email: "fanele.skhosana@mphatek.com", role: "employee", employeeId: 4 },
     { fullName: "Mawelele Busisiwe HP", email: "busisiwe.mawelele@mphatek.com", role: "employee", employeeId: 5 },
-    { fullName: "Samarjeet Singh", email: "samarjeet.signh@mphatek.com", role: "employee", employeeId: 6 },
-    { fullName: "Vinod Savannapelly", email: "yinod.s@mpatek.com", role: "employee", employeeId: 7 },
+    { fullName: "Samarjeet Singh", email: "samarjeet.singh@mphatek.com", role: "employee", employeeId: 6 },
+    { fullName: "Vinod Savannapelly", email: "vinod.s@mpatek.com", role: "employee", employeeId: 7 },
     { fullName: "Busisiwe Zikhalala", email: "busisiwe.zikhalala@mphatek.com", role: "reception", employeeId: 3 }, 
+    { fullName: "Roompam Gupta", email: "roompam.gupta@mphatek.com", role: "employee", employeeId: 10},
+    { fullName: "Shailesh Singh", email: "shailesh.singh@mphatek.com", role: "employee", employeeId: 9}
   ];
 
   for (const u of users) {
     try {
        const passwordHash = u.role === "reception" ? receptionHash : hash;
-      await poolquery(
+      await pool.query(
         `
           INSERT INTO Users (FullName, Email, PasswordHash, Role, IsActive, CreatedDate, HostEmployeeID)
-          VALUES (@fullName, @email, @hash, @role, 1, GETDATE(), @employeeId)
-        `);
+          VALUES ($1, $2, $3, $4, true, NOW(), $5)
+        `,
         [u.fullName, u.email, passwordHash, u.role, u.employeeId]
+      );
       console.log(`✅ User ${u.fullName} seeded successfully.`);
     } catch (err) {
       console.error(`❌ Failed to seed user ${u.fullName}:`, err);
