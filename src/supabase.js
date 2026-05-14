8d6bca7098c687ac5ccb7b4ac7924360ee7b9fe3
Koketso <boipelokoki77@gmail.com>
2026-05-14 10:50:04 +0200

    update supabase anon key code

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import {createClient} from '@supabase/supabase-js';
 
 const supabaseURL = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseURL, supabaseKey);
